eb4ca028855f94da2906c19861016a0fd7976a07
Kriijan <Berlack.Tirlax@hotmail.fr>
2020-05-18 14:04:27 +0200

    Added the item "an_item"

--- HelloWorld/src/main/java/com/github/kriijan/boolcraft/BoolCraft.java
+++ HelloWorld/src/main/java/com/github/kriijan/boolcraft/BoolCraft.java
@@ -1,9 +1,12 @@
 package com.github.kriijan.boolcraft;
 
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+
+import com.github.kriijan.boolcraft.init.ModItems;
 
 @Mod("boolcraft")
 public final class BoolCraft {
@@ -12,5 +15,6 @@ public final class BoolCraft {
 	
 	public BoolCraft() {
 		LOGGER.debug("Hello from BoolCraft !");
+		ModItems.ITEMS.register(FMLJavaModLoadingContext.get().getModEventBus());
 	}
 }
