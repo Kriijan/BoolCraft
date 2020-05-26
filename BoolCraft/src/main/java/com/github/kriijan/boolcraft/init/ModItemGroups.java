64cf4e258d264edcface5d247be9f92fc70b2501
Kriijan <Berlack.Tirlax@hotmail.fr>
2020-05-26 13:59:19 +0200

    Added a ItemGroup to access items in creative inventory

---
+++ BoolCraft/src/main/java/com/github/kriijan/boolcraft/init/ModItemGroups.java
@@ -0,0 +1,28 @@
+package com.github.kriijan.boolcraft.init;
+
+import java.util.function.Supplier;
+
+import com.github.kriijan.boolcraft.BoolCraft;
+
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+
+public class ModItemGroups {
+	public static final ItemGroup MOD_ITEM_GROUP = new ModItemGroup(BoolCraft.MODID, () -> new ItemStack(ModItems.AN_ITEM.get()));
+	
+	public static class ModItemGroup extends ItemGroup {
+		
+		private final Supplier<ItemStack> iconSupplier;
+		
+		public ModItemGroup(final String name, final Supplier<ItemStack> iconSupplier) {
+			super(name);
+			this.iconSupplier = iconSupplier;
+		}
+		
+		@Override
+		public ItemStack createIcon() {
+			return iconSupplier.get();
+		}
+	}
+	
+}
--- BoolCraft/src/main/java/com/github/kriijan/boolcraft/init/ModItems.java
+++ BoolCraft/src/main/java/com/github/kriijan/boolcraft/init/ModItems.java
@@ -9,13 +9,14 @@ import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
 public class ModItems {
+	
 	//Création d'un registre dans lequel enregistrer les items
 	public static final DeferredRegister<Item> ITEMS = new DeferredRegister<>(ForgeRegistries.ITEMS, BoolCraft.MODID);
 	
 	//Enregistrement et création de l'item
-	public static final RegistryObject<Item> AN_ITEM = ITEMS.register("an_item", () -> new Item(new Item.Properties().maxStackSize(64)));
+	public static final RegistryObject<Item> AN_ITEM = ITEMS.register("an_item", () -> new Item(new Item.Properties().maxStackSize(64).group(ModItemGroups.MOD_ITEM_GROUP)));
 	
 	//Enregistrement et création de l'ItemBlock
-	public static final RegistryObject<Item> A_BLOCK = ITEMS.register("a_block", () -> new BlockItem(ModBlocks.A_BLOCK.get(), new Item.Properties().maxStackSize(64)));
+	public static final RegistryObject<Item> A_BLOCK = ITEMS.register("a_block", () -> new BlockItem(ModBlocks.A_BLOCK.get(), new Item.Properties().maxStackSize(64).group(ModItemGroups.MOD_ITEM_GROUP)));
 	
 }
